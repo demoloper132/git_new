a4b2fde507dcb869329965847aced15c8d8cd0d2
mohamed Abdeldayem <demoloper132@gmail.com>
2024-02-15 13:20:05 +0200

    adding file n

---
+++ n.py
@@ -0,0 +1,7 @@
+start = int(input("enter start number :"))
+end = int(input("enter end number :"))
+for x in range(start,end+1):
+    for y in range(start,end+1):
+        result = x* y
+        print (f" {x}X{y} ={result}")
+    print("------------")
